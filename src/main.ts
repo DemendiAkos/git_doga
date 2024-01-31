dc579afb1af76279d09e4609a92316337d751113
DemendiAkos <akosdemendi@gmail.com>
2024-01-31 13:25:16 +0100

    implemented function adding given numbers by the user to array

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 import './style.css'
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  const arr: number[] = [];
+  
+  document.getElementById('numberButton')?.addEventListener('click', () => {
+    let li = document.createElement('li') as HTMLElement;
+    let number = document.getElementById('inputnumber') as HTMLInputElement;
+
+    arr.push(parseInt(number.value));
+    console.log(arr);
+  })
+});
